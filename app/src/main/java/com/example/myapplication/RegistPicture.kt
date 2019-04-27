f4017929b5e48ec9fbd9b8afa4e93d48eede8228
寺田優 <mterada@taraMac.local>
2019-04-27 23:18:12 +0900

    create func to take a pict

--- app/src/main/java/com/example/myapplication/RegistPicture.kt
+++ app/src/main/java/com/example/myapplication/RegistPicture.kt
@@ -1,19 +1,91 @@
 package com.example.myapplication
 
-import android.support.v7.app.AppCompatActivity
+import android.app.PendingIntent.getActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_regist_picture.*
-
+import android.content.DialogInterface
+import android.content.Intent
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.Manifest
+import android.app.Activity
+import android.graphics.Bitmap
 
 
 class RegistPicture : AppCompatActivity() {
+
+    // 選択中の画像url
+    private var url = R.drawable.noimage
+
+    // カメラステータス
+    companion object {
+        const val CAMERA_REQUEST_CODE = 1
+        const val CAMERA_PERMISSION_REQUEST_CODE = 2
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regist_picture)
 
-        Picasso.with(this).load(R.drawable.noimage)
-           .transform(CircleTransform()).into(profileImage)
+        // 初期画像の設定
+        Picasso.with(this).load(url)
+            .transform(CircleTransform()).into(profileImage)
+
+        // パーミッションの取得
+        grantCameraPermission()
+
+        // プロフィール画像の設定ボタンを押下
+        SetProfileImageBtn.setOnClickListener(){
+            showSelector()
+        }
     }
+
+    // 画像設定方法の選択
+    private fun showSelector(){
+        val items = arrayOf("画像を撮影する", "カメラロールから選択", "キャンセル")
+        AlertDialog.Builder(this).apply {
+            setItems(items, DialogInterface.OnClickListener { dialog, which ->
+                if (which == 0) {
+                    // 画像を撮影する押下時
+                    takePicture()
+                    Log.d("[DEBUG]", "画像を撮影する")
+                } else if (which == 1) {
+                    // 画像カメラロールから選択時
+                    selectPicture()
+                    Log.d("[DEBUG]", "カメラロールから選択")
+                }
+            })
+            show()
+        }
+    }
+
+    // 画像撮影
+    private fun takePicture() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            addCategory(Intent.CATEGORY_DEFAULT)
+        }
+        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+    }
+
+    // 撮影した画像への処理
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val image = data?.extras?.get("data")?.let {
+                profileImage.setImageBitmap(it as Bitmap)
+            }
+        }
+    }
+
+    // カメラロールから選択
+    private fun selectPicture() {
+
+    }
+
+    // カメラアプリのパーミッションを取得
+    private fun grantCameraPermission() =
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
 }
