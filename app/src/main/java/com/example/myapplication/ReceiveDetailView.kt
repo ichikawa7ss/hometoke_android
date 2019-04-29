46976e8e7d32abb41784a5b2357b328a5a81f15b
icchii <i2s7.chacha@gmail.com>
2019-04-30 02:39:05 +0900

    褒められた画面、褒められた詳細画面のマージ

---
+++ app/src/main/java/com/example/myapplication/ReceiveDetailView.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_detail_view.*
+
+class ReceiveDetailView : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_view)
+
+
+        // 戻るボタン "<" の作成　詳細不明
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "SubActivity"
+
+        // 褒められた画面から送られてきた情報を取り出す
+        val intent = getIntent()
+
+        // 画面作成
+        stamp_image.setImageResource(intent.getIntExtra("intent_imgid",0))
+        stamp_title.text     = intent.getStringExtra("intent_title")
+        stamp_server.text    = intent.getStringExtra("intent_server")
+        stamp_user_name.text = intent.getStringExtra("intent_user_name")
+        stamp_question.text  = intent.getStringExtra("intent_question")
+
+    }
+
+    // 戻るボタン "<" が押された時の設定
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> finish()
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
+}
