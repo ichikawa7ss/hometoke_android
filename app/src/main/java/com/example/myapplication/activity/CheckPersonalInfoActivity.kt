4dc8db38fd51ea5101571fcaa58830eec4e30d2d
icchii <i2s7.chacha@gmail.com>
2019-05-23 08:33:49 +0900

    画面遷移、チェックボックスの中身の実装

--- app/src/main/java/com/example/myapplication/activity/CheckPersonalInfoActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/CheckPersonalInfoActivity.kt
@@ -3,28 +3,60 @@ package com.example.myapplication.activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import com.example.myapplication.Confirmation
+import android.widget.CheckBox
 import com.example.myapplication.R
+import android.view.View
+import kotlinx.android.synthetic.main.activity_check_personal_info.*
+
 
 class CheckPersonalInfoActivity : AppCompatActivity() {
+
+    var checkClick = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_personal_info)
+
+        val checkbox = findViewById<View>(R.id.checkBox) as CheckBox
+        checkbox.isChecked = false
+
+        // set the listener upon the checkbox
+        checkbox.setOnClickListener(View.OnClickListener
+        {
+            val check = checkbox.isChecked()
+            if (check) {
+                this.checkClick = true
+            } else {
+                this.checkClick = false
+            }
+        })
+
+        ToConfirmButtonmoveToConfirmationBtn.setOnClickListener{
+            moveToConfirmationView()
+        }
     }
 
     fun moveToConfirmationView() {
         println("次へボタン押下")
 
-        // 次へボタンが押下された場合
-        val confirmationIntent = Intent(this, Confirmation::class.java)
+        if (checkClick) {
+            // 同意されていれば画面遷移
+            val confirmationIntent = Intent(this, Confirmation::class.java)
 
-        confirmationIntent.putExtra(
-            "checkInputBirthday",
-            intent.getBooleanExtra("checkImputBirthday",true)
-        )
+            confirmationIntent.putExtra("checkInputBirthday", intent.getBooleanExtra("checkImputBirthday",true))
 
-        startActivity(confirmationIntent)
+            startActivity(confirmationIntent)
+        } else {
+            // 同意がなければ
+            AlertDialog.Builder(this).apply {
+                setTitle("プライバシーポリシーを確認してください")
+                setMessage("このアプリの利用にはプライバシーポリシーへの同意が必要です")
+                setPositiveButton("OK", null)
+                show()
+            }
+        }
     }
 
 }
